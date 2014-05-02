4d8eb3afa687ebbbd226dae43211371f3740878e
Jake Shipley <shipley.jacob@gmail.com>
2014-05-02 11:14:00 -0500

    Updated refresh timing

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var http = require('https');
 var data = '';
 var markets = [];
 var stats;
+var UPDATE_TIMER = 10000;
 
 var options = {
   host: "api.mintpal.com",
@@ -27,42 +28,60 @@ http.request(options, function(res) {
   res.on('end', function() {
     var responseObject = JSON.parse(data);
     for(var i = 0; i < responseObject.length; i++){
-    	var market = responseObject[i];
-    	markets.push(market);
+      var market = responseObject[i];
+      markets.push(market);
     }
     setupMarkets();
   });
 }).end()
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'MintPal Overview',
-  						markets: markets });
+router.get('/', function(req, response) {
+  response.render('index', { title: 'MintPal Overview',
+                             markets: markets });
 });
 
-function setupMarkets(){
-	for(var i = 0; i < markets.length; i++){
-		/* GET markets */
-		var market = markets[i];
-		router.get('/' + i, function(req, response) {
-		  var num = req.url.slice(1,req.url.length);
-		  console.log(num);
-		  var market = markets[num];
-		  var stats = '';
-		  stat_options.path = '/v1/market/trades/' + market.code + '/' + market.exchange;
-		  http.request(stat_options, function(res) {
-			  res.setEncoding('utf8');
-			  res.on('data', function (chunk) {
-			    stats += chunk.toString();
-			  });
-			  res.on('end', function() {
-			  	//stats += ']';
-			    response.render('market', { market: market, stats: stats });
-			  });
-			}).end()
-		});
-	}
-}
+setInterval(function() {
+  markets.length = 0;
+  data = '';
+  http.request(options, function(res) {
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    data += chunk.toString();
+  });
+  res.on('end', function() {
+    var responseObject = JSON.parse(data);
+    for(var i = 0; i < responseObject.length; i++){
+      var market = responseObject[i];
+      markets.push(market);
+    }
+    setupMarkets();
+  });
+  }).end()
+}, UPDATE_TIMER );
 
+function setupMarkets(){
+  for(var i = 0; i < markets.length; i++){
+    /* GET markets */
+    var market = markets[i];
+    router.get('/' + i, function(req, response) {
+      var num = req.url.slice(1,req.url.length);
+      console.log(num);
+      var market = markets[num];
+      var stats = '';
+      stat_options.path = '/v1/market/trades/' + market.code + '/' + market.exchange;
+      http.request(stat_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          stats += chunk.toString();
+        });
+        res.on('end', function() {
+          //stats += ']';
+          response.render('market', { market: market, stats: stats });
+        });
+      }).end()
+    });
+  }
+}
 
 module.exports = router;
